3ee3d8448cf792ccc6485945b8d4296afd5d2ffa
Lucas Nørgård <lucasnrgaard@gmail.com>
2024-02-18 10:58:10 +0100

    test: directly override env

--- test/index.test.ts
+++ test/index.test.ts
@@ -10,13 +10,16 @@ beforeEach(() => {
   Object.defineProperty(process, "platform", {
     value: "linux",
   });
-  vi.unstubAllEnvs();
+  process.env = {};
   process.argv = [];
 });
 
 describe("get supported color mode", () => {
   it("should return `0` if NO_COLOR is in env", () => {
-    vi.stubEnv("NO_COLOR", "1");
+    process.env = {
+      NO_COLOR: "1",
+    };
+
     expect(getSupportedLevel()).toBe(0);
   });
 
@@ -26,7 +29,10 @@ describe("get supported color mode", () => {
   });
 
   it("should return `3` if FORCE_COLOR is in env", () => {
-    vi.stubEnv("FORCE_COLOR", "1");
+    process.env = {
+      FORCE_COLOR: "1",
+    };
+
     expect(getSupportedLevel()).toBe(3);
   });
 
@@ -36,70 +42,106 @@ describe("get supported color mode", () => {
   });
 
   it("return false if `CI` is in env", async () => {
-    vi.stubEnv("CI", "APPVEYOR");
+    process.env = {
+      CI: "1",
+    };
+
     expect(getSupportedLevel()).toBe(0);
   });
 
   it("return true if `TRAVIS` is in env", async () => {
-    vi.stubEnv("CI", "Travis");
-    vi.stubEnv("TRAVIS", "1");
+    process.env = {
+      CI: "true",
+      TRAVIS: "true",
+    };
+
     expect(getSupportedLevel()).toBe(1);
   });
 
   it("return true if `CIRCLECI` is in env", async () => {
-    vi.stubEnv("CI", "true");
-    vi.stubEnv("CIRCLECI", "true");
+    process.env = {
+      CI: "true",
+      CIRCLECI: "true",
+    };
+
     expect(getSupportedLevel()).toBe(1);
   });
 
   it("return true if `APPVEYOR` is in env", async () => {
-    vi.stubEnv("CI", "true");
-    vi.stubEnv("APPVEYOR", "true");
+    process.env = {
+      CI: "true",
+      APPVEYOR: "true",
+    };
+
     expect(getSupportedLevel()).toBe(1);
   });
 
   it("return true if `GITLAB_CI` is in env", async () => {
-    vi.stubEnv("CI", "true");
-    vi.stubEnv("GITLAB_CI", "true");
+    process.env = {
+      CI: "true",
+      GITLAB_CI: "true",
+    };
+
     expect(getSupportedLevel()).toBe(1);
   });
 
   it("return true if `BUILDKITE` is in env", async () => {
-    vi.stubEnv("CI", "true");
-    vi.stubEnv("BUILDKITE", "true");
+    process.env = {
+      CI: "true",
+      BUILDKITE: "true",
+    };
+
     expect(getSupportedLevel()).toBe(1);
   });
 
   it("return true if `DRONE` is in env", async () => {
-    vi.stubEnv("CI", "true");
-    vi.stubEnv("DRONE", "true");
+    process.env = {
+      CI: "true",
+      DRONE: "true",
+    };
+
     expect(getSupportedLevel()).toBe(1);
   });
 
   it("return level 3 if `GITEA_ACTIONS` is in env", async () => {
-    vi.stubEnv("CI", "true");
-    vi.stubEnv("GITEA_ACTIONS", "true");
+    process.env = {
+      CI: "true",
+      GITEA_ACTIONS: "true",
+    };
+
     expect(getSupportedLevel()).toBe(3);
   });
 
   it("return true if Codeship is in env", async () => {
-    vi.stubEnv("CI", "true");
-    vi.stubEnv("CI_NAME", "codeship");
+    process.env = {
+      CI: "true",
+      CI_NAME: "codeship",
+    };
+
     expect(getSupportedLevel()).toBe(1);
   });
 
   it("return false if `TEAMCITY_VERSION` is in env and is < 9.1", async () => {
-    vi.stubEnv("TEAMCITY_VERSION", "9.0.5 (build 32523)");
+    process.env = {
+      TEAMCITY_VERSION: "9.0.5 (build 32523)",
+    };
+
     expect(getSupportedLevel()).toBe(0);
   });
 
   it("return true if `TEAMCITY_VERSION` is in env and is a newer release", async () => {
-    vi.stubEnv("TEAMCITY_VERSION", "2023.11.3 (build 147512)");
+    process.env = {
+      TEAMCITY_VERSION: "2023.11.3 (build 147512)",
+    };
+
     expect(getSupportedLevel()).toBe(1);
   });
 
   it("return level 1 if `TEAMCITY_VERSION` is in env and is >= 9.1", async () => {
-    vi.stubEnv("TEAMCITY_VERSION", "9.1.0 (build 32523)");
+    process.env = {
+      TEAMCITY_VERSION: "9.1.0 (build 32523)",
+    };
+
     expect(getSupportedLevel()).toBe(1);
   });
 });
