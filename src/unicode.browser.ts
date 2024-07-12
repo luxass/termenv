4c51a89afa5866bcc07ca05d41653e55454c194e
Lucas Nørgård <lucasnrgaard@gmail.com>
2024-07-12 09:58:32 +0200

    fix(unicode): expose browser variant

---
+++ src/unicode.browser.ts
@@ -0,0 +1,7 @@
+/**
+ * Checks if the current environment supports Unicode.
+ * @returns {boolean} A boolean value indicating whether Unicode is supported.
+ */
+export function isUnicodeSupported(): boolean {
+  return true;
+}
