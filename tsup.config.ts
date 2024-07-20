4bbb7597c1bde151825d13006fb338c73985a882
Lucas Nørgård <lucasnrgaard@gmail.com>
2024-07-20 13:53:33 +0200

    feat!: remove browser exports

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,45 +1,23 @@
 import { defineConfig } from "tsup";
 
-export default defineConfig([
-  {
-    entry: [
-      "./src/index.ts",
-      "./src/supports.ts",
-      "./src/unicode.ts",
-      "./src/utils.ts",
-      "./src/window-size.ts",
-    ],
-    format: ["cjs", "esm"],
-    platform: "node",
-    target: "es2022",
-    dts: true,
-    treeshake: true,
-    bundle: true,
-    clean: false,
-    outExtension(ctx) {
-      return {
-        js: ctx.format === "cjs" ? ".cjs" : ".mjs",
-      };
-    },
+export default defineConfig({
+  entry: [
+    "./src/index.ts",
+    "./src/supports.ts",
+    "./src/unicode.ts",
+    "./src/utils.ts",
+    "./src/window-size.ts",
+  ],
+  format: ["cjs", "esm"],
+  platform: "node",
+  target: "es2022",
+  dts: true,
+  treeshake: true,
+  bundle: true,
+  clean: false,
+  outExtension(ctx) {
+    return {
+      js: ctx.format === "cjs" ? ".cjs" : ".mjs",
+    };
   },
-  {
-    entry: [
-      "./src/index.browser.ts",
-      "./src/supports.browser.ts",
-      "./src/unicode.browser.ts",
-    ],
-    format: ["cjs", "esm"],
-    platform: "browser",
-    outDir: "dist/browser",
-    target: "es2022",
-    dts: true,
-    treeshake: true,
-    clean: true,
-    bundle: true,
-    outExtension(ctx) {
-      return {
-        js: ctx.format === "cjs" ? ".cjs" : ".mjs",
-      };
-    },
-  },
-]);
+});
