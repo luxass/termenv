7b1a6974a071dac564f17f16ce901c6bdd556b6b
Lucas Nørgård <lucasnrgaard@gmail.com>
2024-07-20 14:09:59 +0200

    feat: export utils and window-size from root module

--- src/index.ts
+++ src/index.ts
@@ -13,3 +13,17 @@ export {
 } from "./supports";
 
 export { isUnicodeSupported } from "./unicode";
+
+export { getWindowSize } from "./window-size";
+
+export {
+  ANSI_REGEX,
+  ERASE_LINE,
+  ERASE_LINE_LEFT,
+  ERASE_LINE_RIGHT,
+  ERASE_SCREEN,
+  ERASE_SCREEN_LEFT,
+  ERASE_SCREEN_RIGHT,
+  RESET,
+  strip,
+} from "./utils";
