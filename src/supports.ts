f794a82aa1c9a7e0b60cf907dbf823d1d771e87b
Lucas Nørgård <lucasnrgaard@gmail.com>
2024-11-09 09:37:21 +0100

    feat: add `getColorSpaceByRuntime` This function will help us detect which color space, the different CI's support, and also which space different terminals support.

--- src/supports.ts
+++ src/supports.ts
@@ -41,7 +41,7 @@ function hasFlag(argv: string[], regex: RegExp): boolean {
   return !!argv.find((arg) => regex.test(arg));
 }
 
-export function getColorSpace(mockGlobal?: typeof globalThis): 0 | 1 | 2 | 3 {
+export function getColorSpace(mockGlobal?: typeof globalThis): ColorSpace {
   let colorSpace = -1;
   const runtime = getRuntimeConfig(mockGlobal);
 
@@ -55,4 +55,80 @@ export function getColorSpace(mockGlobal?: typeof globalThis): 0 | 1 | 2 | 3 {
   }
 
   return colorSpace as ColorSpace;
+}
+
+const TRUE_COLOR_CI = [
+  "GITHUB_ACTIONS",
+  "GITEA_ACTIONS",
+];
+
+export function getColorSpaceByRuntime(env: Record<string, string | undefined>, isTTY: boolean, isWin: boolean): ColorSpace {
+  const { TERM: term } = env;
+
+  // Azure DevOps CI
+  // https://learn.microsoft.com/en-us/azure/devops/pipelines/build/variables?view=azure-devops&tabs=yaml
+  if ("TF_BUILD" in env) return SPACE_16_COLORS;
+
+  // https://youtrack.jetbrains.com/issue/TW-62063/Expand-ANSI-coloring-support-to-include-256-color-set
+  // JetBrains TeamCity support 256 colors since 2020.1.1 (2020-06-23)
+  if ("TEAMCITY_VERSION" in env) return SPACE_256_COLORS;
+
+  // CI tools
+  // https://github.com/watson/ci-info/blob/master/vendors.json
+  if ("CI" in env) {
+    if (TRUE_COLOR_CI.some((key) => key in env)) return SPACE_TRUE_COLORS;
+
+    // others CI supports only 16 colors
+    return SPACE_16_COLORS;
+  }
+
+  // unknown output or colors are not supported
+  if (!isTTY || /-mono|dumb/i.test(term!)) return SPACE_MONO;
+
+  // truecolor support starts from Windows 10 build 14931 (2016-09-21), in 2024 we assume modern Windows is used
+  if (isWin) return SPACE_TRUE_COLORS;
+
+  // if we can't detect the terminal, we assume it supports true colors
+  // because most modern terminals support true colors
+  return SPACE_TRUE_COLORS;
+}
+
+/**
+ * Checks if the terminal supports true color (24-bit color).
+ *
+ * @param {typeof globalThis?} mockGlobal global object for testing purposes
+ * @returns {boolean} Returns true if the terminal supports true color, false otherwise
+ */
+export function isTrueColorSupported(mockGlobal?: typeof globalThis): boolean {
+  return getColorSpace(mockGlobal) === SPACE_TRUE_COLORS;
+}
+
+/**
+ * Determines if the terminal supports 256 colors.
+ *
+ * @param {typeof globalThis?} mockGlobal global object for testing purposes
+ * @returns {boolean} Returns true if the terminal supports 256 colors, false otherwise
+ */
+export function is256ColorSupported(mockGlobal?: typeof globalThis): boolean {
+  return getColorSpace(mockGlobal) >= SPACE_256_COLORS;
+}
+
+/**
+ * Determines if the terminal supports 16 colors.
+ *
+ * @param {typeof globalThis?} mockGlobal global object for testing purposes
+ * @returns {boolean} Returns true if the terminal supports 16 colors, false otherwise
+ */
+export function is16ColorSupported(mockGlobal?: typeof globalThis): boolean {
+  return getColorSpace(mockGlobal) >= SPACE_16_COLORS;
+}
+
+/**
+ * Determines if the terminal or environment supports colored output.
+ *
+ * @param {typeof globalThis?} mockGlobal global object for testing purposes
+ * @returns {boolean} Returns true if colors are supported, false otherwise
+ */
+export function isColorsSupported(mockGlobal?: typeof globalThis): boolean {
+  return getColorSpace(mockGlobal) > SPACE_MONO;
 }
