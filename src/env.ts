d16d55dc3fb60b33a47cd63f9d0d3bede6a71cec
Lucas <lucasnrgaard@gmail.com>
2025-07-14 13:46:09 +0200

    feat(env): extend runtime options in EnvRuntimeConfig
    - Added "browser" and "unknown" as possible values for the `runtime` property.
    - Updated `getRuntimeConfig` function to correctly identify the runtime environment, including browser detection.
    
    resolves #94, #96

--- src/env.ts
+++ src/env.ts
@@ -17,7 +17,7 @@ export interface EnvRuntimeConfig {
   env: Record<string, string | undefined>;
 
   /** The JavaScript runtime environment being used */
-  runtime: "deno" | "node" | "bun";
+  runtime: "deno" | "node" | "bun" | "browser" | "unknown";
 }
 
 /**
@@ -42,27 +42,37 @@ export function getRuntimeConfig<TGlobal = typeof globalThis>(mockGlobal?: TGlob
   const _global = mockGlobal || globalThis;
   const Deno = (_global as any).Deno;
   const Bun = (_global as any).Bun;
+  // eslint-disable-next-line node/prefer-global/process
+  const proc = (_global as any).process || {};
+
+  let env = proc.env ?? {};
+  try {
+    // will trigger a Deno permission request, if running in Deno
+    env = proc.env;
+  } catch {
+    env = {};
+  }
+
   const isDeno = Deno != null;
   const isBun = Bun != null;
+  const isNode = !isDeno && !isBun && typeof proc === "object" && proc?.versions?.node;
+  const isBrowser = !!(_global as any).window?.chrome;
 
-  // @ts-ignore - we fallback to using `{}` so this is fine.
-  // eslint-disable-next-line node/prefer-global/process
-  const proc = _global.process || Deno || {};
-
-  let env: Record<string, string | undefined> = proc.env || {};
-  if (isDeno) {
-    try {
-      env = Deno.env.toObject();
-    } catch {
-      env = {};
-    }
-  }
+  const runtime = isDeno
+    ? "deno"
+    : isBun
+      ? "bun"
+      : isBrowser
+        ? "browser"
+        : isNode
+          ? "node"
+          : "unknown";
 
   return {
     env,
-    isTTY: isDeno ? Deno.isatty(1) : !!(proc.stdout?.isTTY),
-    platform: isDeno ? Deno.build.os : proc.platform,
-    argv: proc.argv || (proc as any).args || [],
-    runtime: isDeno ? "deno" : isBun ? "bun" : "node",
+    isTTY: !!(proc.stdout?.isTTY),
+    argv: proc.argv || [],
+    platform: proc.platform,
+    runtime,
   };
 }
