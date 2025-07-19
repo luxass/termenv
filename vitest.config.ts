2a7eb64954a525a47b86871367bb226d417df36f
Lucas <lucasnrgaard@gmail.com>
2025-07-19 05:15:34 +0200

    test: setup browser tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,14 +2,50 @@ import { defaultExclude, defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    exclude: [
-      "test-deno/**/*.test.ts",
-      "test-bun/**/*.test.ts",
-      ...defaultExclude,
-    ],
     coverage: {
       provider: "v8",
-      include: ["**/src/**"],
+      include: ["src/**/*.ts"],
     },
+    projects: [
+      {
+        test: {
+          name: "unit",
+          exclude: [
+            "test-deno/**/*.test.ts",
+            "test-bun/**/*.test.ts",
+            "test/browser/**.test.ts",
+            ...defaultExclude,
+          ],
+          environment: "node",
+        },
+      },
+      {
+        test: {
+          name: "browser",
+          exclude: [
+            "test-deno/**/*.test.ts",
+            "test-bun/**/*.test.ts",
+            "test/*.test.ts",
+            ...defaultExclude,
+          ],
+          include: [
+            "test/browser/**/*.test.ts",
+          ],
+          browser: {
+            enabled: true,
+            provider: "playwright",
+            instances: [
+              {
+                browser: "chromium",
+                // We can't use headless mode in browser tests, because window.chrome is not defined in headless mode
+                // and therefore fails our color detection logic.
+                headless: false,
+                screenshotFailures: false,
+              },
+            ],
+          },
+        },
+      },
+    ],
   },
 });
